7eedff85938ac6f3e8f027578f26ec7541f78a29
Marco <AmoebeLabs@users.noreply.github.com>
2026-08-17 14:54:50 +0200

    Update const.py

--- custom_components/flex_horseshoe_card/const.py
+++ custom_components/flex_horseshoe_card/const.py
@@ -12,3 +12,8 @@ CONF_HUMIDITY_ENTITY = "humidity_entity"
 CONF_ENERGY_ENTITY = "energy_entity"
 CONF_BATTERY_ENTITY = "battery_entity"
 CONF_SWITCH_ENTITY = "switch_entity"
+
+DEMO_SOURCE_DIR = "demo"
+DEMO_TARGET_DIR = "fhs_demo"
+DEMO_DASHBOARD_FILE = "fhs-demo-dashboard.yaml"
+DEMO_VIEWS_DIR = "views"

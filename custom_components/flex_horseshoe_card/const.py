da0df2a57befb8b938b21a6b913004b92754751c
Marco <AmoebeLabs@users.noreply.github.com>
2026-08-17 14:00:49 +0200

    Add configuration constants for entities

--- custom_components/flex_horseshoe_card/const.py
+++ custom_components/flex_horseshoe_card/const.py
@@ -2,3 +2,11 @@ DOMAIN = "flex_horseshoe_card"
 
 FRONTEND_PATH = "/fhs"
 FRONTEND_FILE = "flex-horseshoe-card.js"
+
+CONF_DEMO_DASHBOARD = "demo_dashboard"
+
+CONF_TEMPERATURE_ENTITY = "temperature_entity"
+CONF_HUMIDITY_ENTITY = "humidity_entity"
+CONF_ENERGY_ENTITY = "energy_entity"
+CONF_BATTERY_ENTITY = "battery_entity"
+CONF_SWITCH_ENTITY = "switch_entity"

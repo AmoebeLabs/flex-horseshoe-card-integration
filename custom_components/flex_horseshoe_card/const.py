b94d0f56f242e0a0f3579673318150911e381533
Marco <AmoebeLabs@users.noreply.github.com>
2026-08-18 12:11:02 +0200

    Replace energy_entity with power_entity constant

--- custom_components/flex_horseshoe_card/const.py
+++ custom_components/flex_horseshoe_card/const.py
@@ -15,6 +15,6 @@ CONF_DEMO_DASHBOARD = "demo_dashboard"
 
 CONF_TEMPERATURE_ENTITY = "temperature_entity"
 CONF_HUMIDITY_ENTITY = "humidity_entity"
-CONF_ENERGY_ENTITY = "energy_entity"
+CONF_POWER_ENTITY = "power_entity"
 CONF_BATTERY_ENTITY = "battery_entity"
 CONF_SWITCH_ENTITY = "switch_entity"

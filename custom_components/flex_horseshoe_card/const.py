9c2a8d23072da42f85c6b63d328392b7f53f11e9
Marco <AmoebeLabs@users.noreply.github.com>
2026-08-18 08:33:32 +0200

    Add demo strategy and dashboard file constants

--- custom_components/flex_horseshoe_card/const.py
+++ custom_components/flex_horseshoe_card/const.py
@@ -4,6 +4,12 @@ DOMAIN = "flex_horseshoe_card"
 
 FRONTEND_PATH = "/fhs"
 FRONTEND_FILE = "flex-horseshoe-card.js"
+DEMO_STRATEGY_FILE = "fhs-demo-strategy.js"
+
+DEMO_SOURCE_DIR = "demo"
+DEMO_DASHBOARD_FILE = "fhs-demo-dashboard.yaml"
+
+WS_DEMO_DASHBOARD = f"{DOMAIN}/demo_dashboard"
 
 CONF_DEMO_DASHBOARD = "demo_dashboard"
 
@@ -12,8 +18,3 @@ CONF_HUMIDITY_ENTITY = "humidity_entity"
 CONF_ENERGY_ENTITY = "energy_entity"
 CONF_BATTERY_ENTITY = "battery_entity"
 CONF_SWITCH_ENTITY = "switch_entity"
-
-DEMO_SOURCE_DIR = "demo"
-DEMO_TARGET_DIR = "fhs_demo"
-DEMO_DASHBOARD_FILE = "fhs-demo-dashboard.yaml"
-DEMO_VIEWS_DIR = "views"
